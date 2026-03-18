3dee3b1f6e1c9df152ad9ec338bb51f42363c966
Aaryaveer Sharma <legend159980@gmail.com>
2026-03-18 12:08:39 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,10 @@
-'use client'
-
 export const dynamic = 'force-dynamic'
-export const fetchCache = 'force-no-store'
+
+import LoginClient from './LoginClient'
+
+export default function Page() {
+  return <LoginClient />
+}
 
 import Image from 'next/image';
 import Link from 'next/link';
